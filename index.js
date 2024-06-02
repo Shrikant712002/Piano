a76daf79b08ddcd7a37ce446122c7ce8697c6693
Shrikant712002 <123623612+Shrikant712002@users.noreply.github.com>
2024-06-02 14:43:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+const pianoKeys = document.querySelectorAll('.key')
+
+function playSound(newUrl){
+    new Audio(newUrl).play()
+}
+
+pianoKeys.forEach((pianoKey,i)=>{
+    const newUrl = 'keys/key' + (i+1) + '.mp3'
+    pianoKey.addEventListener('click', () => playSound(newUrl))
+})
